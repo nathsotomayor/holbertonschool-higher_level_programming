39b5ff28d28fe3196c2933ce85937444587967ba
Nathaly <nathysoto12.07@gmail.com>
2020-04-29 04:04:23 +0000

    Fix indent

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,4 @@
 for numbers1 in range(0, 10):
     for numbers2 in range(numbers1 + 1, 10):
         print("{:d}{:d}".format(numbers1, numbers2),
-end="\n" if numbers1 == 8 and numbers1 + 1 == 9 else ", ")
+              end="\n" if numbers1 == 8 and numbers1 + 1 == 9 else ", ")
