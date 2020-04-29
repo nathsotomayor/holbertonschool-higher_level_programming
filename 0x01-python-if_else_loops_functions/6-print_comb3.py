3b530a268db52838f53b186ac53ee301c5a53f3e
Nathaly <nathysoto12.07@gmail.com>
2020-04-29 02:44:26 +0000

    Print diferents combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for numbers1 in range(0, 10):
+    for numbers2 in range(numbers1 + 1, 10):
+        print("{:d}{:d}".format(numbers1, numbers2),
+end="\n" if numbers1 == 8 and numbers1 + 1 == 9 else ", ")
