3d1ef44ba5a69da89dcb295166a275137c32867b
Nathaly <nathysoto12.07@gmail.com>
2020-04-30 23:37:43 +0000

    Add conditionals

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{:d} arguments: ".format(len(sys.argv) - 1))
+    if len(sys.argv) - 1 == 1:
+        print("{:d} argument: ".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments: ".format(len(sys.argv) - 1))
     num_args = len(sys.argv)
     for i in range(1, num_args):
         print("{:d}: {}".format(i, str(sys.argv[i])))
