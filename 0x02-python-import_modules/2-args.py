ebfb852340932c7eb2d1d57f3fcbaaa9b3b501c7
Nathaly <nathysoto12.07@gmail.com>
2020-04-30 23:56:23 +0000

    Fix conditionals

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) - 1 == 1:
         print("{:d} argument:".format(len(sys.argv) - 1))
-    else:
+    elif len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
     num_args = len(sys.argv)
     for i in range(1, num_args):
         print("{:d}: {}".format(i, str(sys.argv[i])))
