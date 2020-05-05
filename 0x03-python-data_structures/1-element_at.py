6f11095ab6e13ca3fc45df7079c10a1741b9eaf6
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-05 23:27:30 +0000

    Retrieves and element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@ def element_at(my_list, idx):
         idx = my_list.index(idx)
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx > len(my_list)-1:
         return None
     return idx
