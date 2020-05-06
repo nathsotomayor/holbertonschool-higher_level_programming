3df99b6afafffc2d7973d164e38fa20f773e851d
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-06 05:32:54 +0000

    Returns tuple with lenght and first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
+    if sentence == "":
         sentence[0] = None
     len_tup = len(sentence)
     char_tup = sentence[0]
