2c654acf2d4134d2b3e361bbbc5c52c6e58d76b7
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-07 23:07:07 +0000

    Computes square values of matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/user/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix_pow = [i[:] for i in matrix]
+    for i in range(0, len(matrix)):    
+        for j in range(0, len(matrix[i])):
+            matrix_pow[i][j] = matrix[i][j] ** 2
+    return matrix_pow
