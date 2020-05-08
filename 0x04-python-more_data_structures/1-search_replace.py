654cb92a677e16d2c17d4a272b61e64b0af09de0
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-08 00:17:06 +0000

    Fix pep8

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy() 
+    new_list = my_list.copy()
     element = 0
     for i in my_list:
         if i == search:
