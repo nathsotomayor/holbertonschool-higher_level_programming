a669c16bdc25896f9f3afda0a619faa0c972199c
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-08 04:43:51 +0000

    Fix pep8

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = {value: a_dictionary[value] * 2  for value in a_dictionary} 
-    return new_dictionary
+    new_dic = {value: a_dictionary[value] * 2 for value in a_dictionary}
+    return new_dic
