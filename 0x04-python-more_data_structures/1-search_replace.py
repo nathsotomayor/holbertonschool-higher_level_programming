660577bee1e1851e81a94ca162664546e2a2de63
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-08 00:04:48 +0000

    Replaces all ocurrences of an element

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -5,5 +5,7 @@ def search_replace(my_list, search, replace):
     for i in my_list:
         if i == search:
             new_list[element] = replace
+        else:
+            new_list[element] = my_list[element]
         element += 1
     return new_list
