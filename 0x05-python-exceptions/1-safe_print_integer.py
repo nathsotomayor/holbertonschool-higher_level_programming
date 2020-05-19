716b1edeef9b1d9060b0e8d3b7974a74429524a7
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-19 00:07:47 +0000

    Prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = int(value)
-        print("{:d}".format(value))
+        if value == int(value):
+            print("{:d}".format(value))
         return True
     except:
         return False
