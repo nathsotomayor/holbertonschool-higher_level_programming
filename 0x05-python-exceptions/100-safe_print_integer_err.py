9d07cd83e818cada34d3e8754e35240df91ec390
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-19 05:34:36 +0000

    Fix print to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
+    except ValueError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
     except:
-        sys.stderr.write("Exception:")
         return False
