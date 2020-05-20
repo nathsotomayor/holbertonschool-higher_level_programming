afff1aadd339d2497fcb749e27435b6032db5648
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-20 00:12:05 +0000

    Defines a square with size instace

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Creates a class Square """
+
+
+class Square:
+    """ Defining a Square class and one instance """
+    def __init__(self, size):
+        """ Initialize instance size of square class """
+        self.__size = size
