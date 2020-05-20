9ee565633e994fd6f573a155e2f05894d1afdb15
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-20 03:26:37 +0000

    Access and update private attribute 'size'

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Creates a class Square """
+
+
+class Square:
+    """ Defining a Square class and one instance """
+    def __init__(self, size=0):
+        """ Initialize instance size of square class """
+        if size is not int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Return the square area """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ Return square size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Update size attribute """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
