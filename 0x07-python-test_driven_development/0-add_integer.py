6f3c644676413aa96a1c413e4c6a411e7f0ac171
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-05-23 04:11:47 +0000

    Integers addition (function and test cases)

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Creates a function that adds 2 integers """
+
+
+def add_integer(a, b=98):
+    """ Defines the function """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    return a + b
