f0af84e0e33ec5c695ba039a10e0020e019724f2
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-03 23:30:00 +0000

    Fix filename

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -5,8 +5,6 @@
 def number_of_lines(filename=""):
     """ Defines the function """
     lines = 0
-    f = open('my_file_0.txt', 'r', encoding='utf-8')
-    with open('my_file_0.txt', 'r', encoding='utf-8') as file_n:
-        lines = len(f.readlines())
+    with open(filename, 'r', encoding='utf-8') as file_n:
+        lines = len(file_n.readlines())
     return lines
-
