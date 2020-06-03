fddbca238e379f29bd7bac70a66b1383ecbb819c
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-03 04:00:39 +0000

    Method that raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Creates a class BaseGeometry """
+
+
+class BaseGeometry:
+    """ Defining the class """
+    def area(self):
+        raise Exception("area() is not implemented")
