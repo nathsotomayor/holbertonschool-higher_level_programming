3b84e563c508a55e3972af9caea77da3ee71efd6
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-05 21:23:35 +0000

    Fix pep8 style

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,9 +4,8 @@
 
 def matrix_divided(matrix, div):
     """ Defines function that divides elements of a matrix """
-    #msg_error_matrix = ""
     if type(matrix) is not list or len(matrix) == 0:
-        raise TypeError("matrix must be a matrix (list of lists)"\
+        raise TypeError("matrix must be a matrix (list of lists)"
                         " of integers/floats")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
@@ -16,6 +15,6 @@ def matrix_divided(matrix, div):
         if len(row) is not len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
         if not all([isinstance(j, (int, float)) for j in row]):
-            raise TypeError("matrix must be a matrix (list of lists)"\
+            raise TypeError("matrix must be a matrix (list of lists)"
                             " of integers/floats")
     return [[round(i / div, 2) for i in row] for row in matrix]
