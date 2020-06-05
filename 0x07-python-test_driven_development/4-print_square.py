ce42678593719749e5d2353b766ad5aa4828e736
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-05 23:05:45 +0000

    Add docstrings

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
-"""[summary]
-"""
+""" Creates a function that prints a square with the character '#' """
 
 
 def print_square(size):
-    """[summary]
-
-    Args:
-        size ([type]): [description]
-    """
+    """ Defines the function """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise TypeError("size must be >= 0")
-    for i in range(size):
-        for i in range(size):
+    for x in range(size):
+        for x in range(size):
             print("#", end='')
         print()
-        
