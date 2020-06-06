71bb0213bfaa4db836c806f718094cf891017477
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-06 03:38:05 +0000

    Prints a text with 2 new lines after '.' '?' ':' characters

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Creates a function that prints a text with two lines """
+
+
+def text_indentation(text):
+    """ Defines the function """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    line = True
+    for msg in text:
+        if (line):
+            if (msg == " "):
+                continue
+            else:
+                line = False
+        print(msg, end="")
+        if msg in [".",":","?"]:
+            print("", end="\n\n")
+            line = True
