e90cb22eb90f436db961e3acae0a2810bcac90ae
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-09 02:44:58 +0000

    Creates Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+""" Creates a Base class that is the base of all other classes """
+
+
+class Base:
+    """ Initializing Base class """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ Initializing constructor """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+    
