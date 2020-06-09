448a37181699203e4cc4ec349760d2633e1b1a5d
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-09 18:49:18 +0000

    Fix pep8 style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -105,5 +105,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """ Representation of the Rectangle """
-        rep_rect = "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
-        return rep_rect
+        return "[Rectangle] ({}) {}/{} - {}/"\
+               "{}".format(self.id, self.x, self.y, self.width, self.height)
