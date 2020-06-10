08365e14f8e49126a2b7c255e0221f959f4eac6b
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-10 04:33:45 +0000

    JSON string repr of list_objs

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,9 +15,21 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ JSON string representation of dictionaries """
         if list_dictionaries:
             return json.dumps(list_dictionaries)
         else:
             return "[]"
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ JSON string representation to a file """
+        json_file = "{}.json".format(cls.__name__)
+        if list_objs is None:
+            new_dict = []
+        else:
+            new_dict = [item.to_dictionary() for item in list_objs]
+        with open(json_file, 'w', encoding='utf-8') as json_file:
+            json_file.write(cls.to_json_string(new_dict))
