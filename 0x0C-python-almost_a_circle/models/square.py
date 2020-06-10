27fa17abc73ed8c2a3a92080d9837d3067cdbf8d
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-06-10 00:57:24 +0000

    Square class inherits from Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Creates a Square class inherits from Rectangle """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ Defining Square class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializing constructor """
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ Gets the size attribute"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Sets the size attribute """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
