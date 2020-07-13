3d273fecab8db8c9e529371a2df78cf96d72f821
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-07-13 23:04:23 +0000

    Create other table and add multiple rows

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- Create a table in the current database
+-- Add multiple rows to new table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (1, 'Jhon', 10); 
+INSERT INTO second_table VALUES (2, 'Alex', 3); 
+INSERT INTO second_table VALUES (3, 'Bob', 14); 
+INSERT INTO second_table VALUES (4, 'George', 8); 
