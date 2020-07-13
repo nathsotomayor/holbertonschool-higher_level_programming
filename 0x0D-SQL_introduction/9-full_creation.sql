1cfc1c0e97de2b7eb51436e9fa63829d0c87d87e
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-07-13 23:22:08 +0000

    Change INSERT in one linex

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
 -- Create a table in the current database
 -- Add multiple rows to new table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (1, 'Jhon', 10); 
-INSERT INTO second_table VALUES (2, 'Alex', 3); 
-INSERT INTO second_table VALUES (3, 'Bob', 14); 
-INSERT INTO second_table VALUES (4, 'George', 8); 
+INSERT INTO second_table
+    (id, name, score)
+VALUES
+    (1, 'Jhon', 10), 
+    (2, 'Alex', 3), 
+    (3, 'Bob', 14), 
+    (4, 'George', 8); 
