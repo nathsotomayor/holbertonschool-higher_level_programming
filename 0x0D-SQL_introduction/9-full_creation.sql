476230884be78df2a933b4d58eb35a516eeb33bd
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-07-13 23:24:40 +0000

    Add some characters

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- Create a table in the current database
 -- Add multiple rows to new table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table
-    (id, name, score)
+INSERT INTO `second_table`
+    (`id`, `name`, `score`)
 VALUES
     (1, 'Jhon', 10), 
     (2, 'Alex', 3), 
