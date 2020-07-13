55f55f56e42a7ca198e0f7af95b608b5a7844ec3
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-07-13 23:26:50 +0000

    Change spaces

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,9 @@
 -- Create a table in the current database
 -- Add multiple rows to new table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO `second_table`
-    (`id`, `name`, `score`)
+INSERT INTO `second_table` (`id`, `name`, `score`)
 VALUES
-    (1, 'Jhon', 10), 
-    (2, 'Alex', 3), 
-    (3, 'Bob', 14), 
-    (4, 'George', 8); 
+(1, 'Jhon', 10), 
+(2, 'Alex', 3), 
+(3, 'Bob', 14), 
+(4, 'George', 8); 
