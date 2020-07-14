7a4956c279efaf1427bad8c7aad48a4e5c67d41d
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-07-14 17:49:17 +0000

    Add sentence

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists all privileges of MySQL users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
