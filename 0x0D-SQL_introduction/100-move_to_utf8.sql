4a15ae353c9e8c57d80c7b18a6e4d7bae104c3df
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-07-14 01:20:26 +0000

    Fix UTF8 format error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Converts database and table to UTF8
 USE hbtn_0c_0;
+ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table COLLATE utf8mb4_general_ci;
