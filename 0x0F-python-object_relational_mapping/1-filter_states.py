020d585ae3202b2541c1e766ca5069781a06f689
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-08 15:39:51 +0000

    Adding __main__

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""Lists all states with a name starting with N (upper N)"""
 
 import sys
 import MySQLdb
 
-# Connecting to database
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-# Creating cursor
-cur = db.cursor()
-# Executing query
-cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id")
-# Obtaining query results
-rows = cur.fetchall()
-for row in rows:
-    print("(%s, '%s')" % row)
-# Close all cursors
-cur.close()
-# Close all databases
-db.close()
+
+if __name__ == "__main__":
+    # Connecting to database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    # Creating cursor
+    cur = db.cursor()
+    # Executing query
+    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id")
+    # Obtaining query results
+    rows = cur.fetchall()
+    for row in rows:
+        print("(%s, '%s')" % row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
