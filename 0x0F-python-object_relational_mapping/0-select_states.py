a1222bcf5afde92c38b4943f4a14968031e561f3
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-08 15:35:48 +0000

    Adding __main__

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,18 +4,21 @@
 import sys
 import MySQLdb
 
-# Connecting to database
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-# Creating cursor
-cur = db.cursor()
-# Executing query
-cur.execute("SELECT * FROM states ORDER BY id")
-# Obtaining query results
-rows = cur.fetchall()
-for row in rows:
-    print("(%s, '%s')" % row)
-# Close all cursors
-cur.close()
-# Close all databases
-db.close()
+
+if __name__ == "__main__":
+    # Connecting to database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    # Creating cursor
+    cur = db.cursor()
+    # Executing query
+    cur.execute("SELECT * FROM states ORDER BY id")
+    # Obtaining query results
+    rows = cur.fetchall()
+    for row in rows:
+        print("(%s, '%s')" % row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
