5e45df48ebe5908119268c2782f1698d6f9ac41e
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-08 15:56:52 +0000

    Change docstring, adding __main__

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""Displays all values in the states table of hbtn_0e_0_usa where name matches
+the argument"""
 
 import sys
 import MySQLdb
 
-# Connecting to database
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-# Creating cursor
-cur = db.cursor()
-# Executing query
-nameArg = sys.argv[4]
-cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(nameArg))
-# Obtaining query results
-rows = cur.fetchall()
-for row in rows:
-    print("(%s, '%s')" % row)
-# Close all cursors
-cur.close()
-# Close all databases
-db.close()
+
+if __name__ == "__main__":
+    # Connecting to database
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    # Creating cursor
+    cur = db.cursor()
+    # Executing query
+    nameArg = sys.argv[4]
+    cur.execute("""SELECT * FROM states WHERE name = '{}'
+                ORDER BY id""".format(nameArg))
+    # Obtaining query results
+    rows = cur.fetchall()
+    for row in rows:
+        print("(%s, '%s')" % row)
+
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
