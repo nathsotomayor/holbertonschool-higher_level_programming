0a80e90436f44f32550f39e86a0c423655be79d0
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-09 04:20:31 +0000

    class definition of a State

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Create a class definition of a State and an instance Base =
+declarative_base() with SQLAlchemy"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Defining class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(128), nullable=False)
