f395d584aa4758b0063afccb2b59293e6f445928
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-09 22:03:56 +0000

    Adds the State object 'Louisiana'

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Adds the State object 'Louisiana' to the database hbtn_0e_6_usa"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    add_state = State(name="Louisiana")
+    session.add(add_state)
+    session.commit()
+
+    session.close()
