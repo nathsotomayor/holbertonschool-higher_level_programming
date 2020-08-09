fc12a92f363091a041107a57261837bdc840d547
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-09 19:07:28 +0000

    Fix pep8 style (line too long)

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Lists all State objects that contain the letter 'a' from the database hbtn_0e_6_usa"""
+"""Lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
