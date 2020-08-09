beb838c198264c677fc185958307f97f2cb2761c
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-09 04:24:00 +0000

    Fix pep8 style (spaces)

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,4 +13,4 @@ class State(Base):
     """Defining class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name =  Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False)
