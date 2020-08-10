d0961157c330fb285791851095c1075d4d6696bd
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-10 16:57:11 +0000

    Fix some code

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Create a class definition of a City"""
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
