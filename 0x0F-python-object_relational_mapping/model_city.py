31198ea3c634e902cfd33c0deb67c4f01d4b2442
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-10 16:44:27 +0000

    Fix some code

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Create a class definition of a State and an instance Base =
-declarative_base() with SQLAlchemy"""
+"""Create a class definition of a City"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +10,7 @@ Base = declarative_base()
 
 class City(Base):
     """Defining class"""
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
