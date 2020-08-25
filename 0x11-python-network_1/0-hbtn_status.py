f217ef4906a433fbf6798632a880c19ccfc786fa
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-24 20:05:09 -0500

    Fix tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,8 @@
 
 import urllib.request
 with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("""Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}""".
-          format(type(html), html, html.decode('utf-8')))
-    # print(type(html))
-    # print(html)
-    # print(html.decode('utf-8'))
+    res = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode('utf-8')))
