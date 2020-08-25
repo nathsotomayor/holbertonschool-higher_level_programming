894001b6d8ae21567eed578020b7c65dd7ce1394
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-24 23:45:44 -0500

    Response header value with 'requests' package

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Response header value with 'requests' package"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    print(req.headers['X-Request-Id'])
