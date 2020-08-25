54035399f3055ab37e91664c614a06cdbd8ce72f
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-24 20:08:25 -0500

    Add no execution when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,12 @@
 """Fetches url 'https://intranet.hbtn.io/status' with 'urllib'"""
 
 import urllib.request
-with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
-    res = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(res)))
-    print("\t- content: {}".format(res))
-    print("\t- utf8 content: {}".format(res.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('http://intranet.hbtn.io/status') as response:
+        res = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
