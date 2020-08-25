facea10f8a68205020ef8d7f65cc1e4a49d8a46c
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-08-24 20:30:18 -0500

    Upload file again

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Fetches url https://intranet.hbtn.io/status with urllib"""
+
+from urllib import request
+
+
+if __name__ == "__main__":
+
+
+    req = request.Request('http://intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        res = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
