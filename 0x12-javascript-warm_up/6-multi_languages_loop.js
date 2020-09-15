815f109a4ccaf697ad5ca71f14af65a22d84b487
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-15 15:38:44 -0500

    Loop to languages Prints 3 lines using an array of string and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+let idx;
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (idx = 0; idx < languages.length; idx++) {
+  console.log(languages[idx]);
+}
