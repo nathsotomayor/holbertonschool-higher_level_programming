0cfdeacc4890881c6cd5bd580580145b46097539
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-15 12:42:47 -0500

    Prints 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let cLanguage = 'C is fun';
+let pyLanguage = 'Python is cool';
+let jsLanguage = 'Javascript is amazing';
+console.log(cLanguage);
+console.log(pyLanguage);
+console.log(jsLanguage);
