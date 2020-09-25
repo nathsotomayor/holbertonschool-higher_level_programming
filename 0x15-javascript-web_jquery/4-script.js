42fce001e665ccd5bddae066c3592c3e32dee7c4
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-24 20:08:32 -0500

    Toggle classes with jQuery

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$("div#toggle_header").click(function() {
+    if ($("header").hasClass("green")) {
+      $("header").removeClass("green");
+      $("header").addClass("red");
+    }
+    else {
+      $("header").removeClass("red");
+      $("header").addClass("green");
+    }
+});
