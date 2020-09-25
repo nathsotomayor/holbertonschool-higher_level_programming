3dabca9ce858b3ab1b56e62c76a20526f3d75b72
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-24 23:51:32 -0500

    Fix semistandard style

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,3 +1,3 @@
-$("div#red_header").click(function() {
-  $("header").addClass("red");
+$('div#red_header').click(function () {
+  $('header').addClass('red');
 });
--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,10 +1,9 @@
-$("div#toggle_header").click(function() {
-    if ($("header").hasClass("green")) {
-      $("header").removeClass("green");
-      $("header").addClass("red");
-    }
-    else {
-      $("header").removeClass("red");
-      $("header").addClass("green");
-    }
+$('div#toggle_header').click(function () {
+  if ($('header').hasClass('green')) {
+    $('header').removeClass('green');
+    $('header').addClass('red');
+  } else {
+    $('header').removeClass('red');
+    $('header').addClass('green');
+  }
 });
